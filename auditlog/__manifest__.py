929464094550eb018567ef669ae2e72e0175888b
tack-yabeQuartile <yabe@quartile.co>
2022-03-31 16:08:00 +0800

    apply V15 module

--- auditlog/__manifest__.py
+++ auditlog/__manifest__.py
@@ -1,24 +1,21 @@
-# -*- coding: utf-8 -*-
-# © 2015 ABF OSIELL <http://osiell.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2015 ABF OSIELL <https://osiell.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': "Audit Log",
-    'version': "10.0.1.0.1",
-    'author': "ABF OSIELL,Odoo Community Association (OCA)",
-    'license': "AGPL-3",
-    'website': "http://www.osiell.com",
-    'category': "Tools",
-    'depends': [
-        'base',
+    "name": "Audit Log",
+    "version": "15.0.1.0.0",
+    "author": "ABF OSIELL, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "category": "Tools",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_cron.xml",
+        "views/auditlog_view.xml",
+        "views/http_session_view.xml",
+        "views/http_request_view.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/ir_cron.xml',
-        'views/auditlog_view.xml',
-        'views/http_session_view.xml',
-        'views/http_request_view.xml',
-    ],
-    'application': True,
-    'installable': True,
+    "application": True,
+    "installable": True,
 }
--- auditlog/models/log.py
+++ auditlog/models/log.py
@@ -1,46 +1,100 @@
-# -*- coding: utf-8 -*-
-# © 2015 ABF OSIELL <http://osiell.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, fields
+# Copyright 2015 ABF OSIELL <https://osiell.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class AuditlogLog(models.Model):
-    _name = 'auditlog.log'
+    _name = "auditlog.log"
     _description = "Auditlog - Log"
     _order = "create_date desc"
 
     name = fields.Char("Resource Name", size=64)
     model_id = fields.Many2one(
-        'ir.model', string=u"Model")
-    res_id = fields.Integer(u"Resource ID")
-    user_id = fields.Many2one(
-        'res.users', string=u"User")
-    method = fields.Char(u"Method", size=64)
-    line_ids = fields.One2many(
-        'auditlog.log.line', 'log_id', string=u"Fields updated")
-    http_session_id = fields.Many2one(
-        'auditlog.http.session', string=u"Session")
-    http_request_id = fields.Many2one(
-        'auditlog.http.request', string=u"HTTP Request")
+        "ir.model", string="Model", index=True, ondelete="set null"
+    )
+    model_name = fields.Char(readonly=True)
+    model_model = fields.Char(string="Technical Model Name", readonly=True)
+    res_id = fields.Integer("Resource ID")
+    user_id = fields.Many2one("res.users", string="User")
+    method = fields.Char(size=64)
+    line_ids = fields.One2many("auditlog.log.line", "log_id", string="Fields updated")
+    http_session_id = fields.Many2one("auditlog.http.session", string="Session")
+    http_request_id = fields.Many2one("auditlog.http.request", string="HTTP Request")
     log_type = fields.Selection(
-        [('full', u"Full log"),
-         ('fast', u"Fast log"),
-         ],
-        string=u"Type")
+        [("full", "Full log"), ("fast", "Fast log")], string="Type"
+    )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Insert model_name and model_model field values upon creation."""
+        for vals in vals_list:
+            if not vals.get("model_id"):
+                raise UserError(_("No model defined to create log."))
+            model = self.env["ir.model"].sudo().browse(vals["model_id"])
+            vals.update({"model_name": model.name, "model_model": model.model})
+        return super().create(vals_list)
+
+    def write(self, vals):
+        """Update model_name and model_model field values to reflect model_id
+        changes."""
+        if "model_id" in vals:
+            if not vals["model_id"]:
+                raise UserError(_("The field 'model_id' cannot be empty."))
+            model = self.env["ir.model"].sudo().browse(vals["model_id"])
+            vals.update({"model_name": model.name, "model_model": model.model})
+        return super().write(vals)
 
 
 class AuditlogLogLine(models.Model):
-    _name = 'auditlog.log.line'
+    _name = "auditlog.log.line"
     _description = "Auditlog - Log details (fields updated)"
 
     field_id = fields.Many2one(
-        'ir.model.fields', ondelete='cascade', string=u"Field", required=True)
+        "ir.model.fields", ondelete="set null", string="Field", index=True
+    )
     log_id = fields.Many2one(
-        'auditlog.log', string=u"Log", ondelete='cascade', index=True)
-    old_value = fields.Text(u"Old Value")
-    new_value = fields.Text(u"New Value")
-    old_value_text = fields.Text(u"Old value Text")
-    new_value_text = fields.Text(u"New value Text")
-    field_name = fields.Char(u"Technical name", related='field_id.name')
-    field_description = fields.Char(
-        u"Description", related='field_id.field_description')
+        "auditlog.log", string="Log", ondelete="cascade", index=True
+    )
+    old_value = fields.Text()
+    new_value = fields.Text()
+    old_value_text = fields.Text("Old value Text")
+    new_value_text = fields.Text("New value Text")
+    field_name = fields.Char("Technical name", readonly=True)
+    field_description = fields.Char("Description", readonly=True)
+    # From log auditlog.log
+    name = fields.Char(related="log_id.name", store=True)
+    model_id = fields.Many2one(related="log_id.model_id", store=True)
+    model_name = fields.Char(related="log_id.model_name", store=True)
+    model_model = fields.Char(related="log_id.model_model", store=True)
+    res_id = fields.Integer(related="log_id.res_id", store=True)
+    user_id = fields.Many2one(related="log_id.user_id", store=True)
+    method = fields.Char(related="log_id.method", store=True)
+    http_session_id = fields.Many2one(related="log_id.http_session_id", store=True)
+    http_request_id = fields.Many2one(related="log_id.http_request_id", store=True)
+    log_type = fields.Selection(related="log_id.log_type", store=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Ensure field_id is not empty on creation and store field_name and
+        field_description."""
+        for vals in vals_list:
+            if not vals.get("field_id"):
+                raise UserError(_("No field defined to create line."))
+            field = self.env["ir.model.fields"].sudo().browse(vals["field_id"])
+            vals.update(
+                {"field_name": field.name, "field_description": field.field_description}
+            )
+        return super().create(vals_list)
+
+    def write(self, vals):
+        """Ensure field_id is set during write and update field_name and
+        field_description values."""
+        if "field_id" in vals:
+            if not vals["field_id"]:
+                raise UserError(_("The field 'field_id' cannot be empty."))
+            field = self.env["ir.model.fields"].sudo().browse(vals["field_id"])
+            vals.update(
+                {"field_name": field.name, "field_description": field.field_description}
+            )
+        return super().write(vals)
