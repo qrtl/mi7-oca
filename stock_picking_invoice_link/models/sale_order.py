6591a84e459909393ad272970244eb22c6f7cd1f
Yoshi Tashiro <tashiro@quartile.co>
2022-07-25 08:56:06 +0000

    incorporate changes from stock-logistics-workflow/pull/750

--- stock_picking_invoice_link/models/sale_order.py
+++ stock_picking_invoice_link/models/sale_order.py
@@ -4,29 +4,59 @@
 # Copyright 2022 Antony Herrera - LooErp
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import models
-from odoo.tools import float_compare
+from odoo.tools import float_compare, float_is_zero
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def get_stock_moves_link_invoice(self):
-        return self.mapped("move_ids").filtered(
-            lambda mv: (
-                mv.state == "done"
-                and not (
-                    any(
-                        inv.state != "cancel"
-                        for inv in mv.invoice_line_ids.mapped("move_id")
+        # >>> QRTL: Incorporating the update from https://github.com/OCA/stock-logistics-workflow/pull/750  # noqa
+        moves_linked = self.env["stock.move"]
+        for stock_move in self.move_ids:
+            if (
+                stock_move.state != "done"
+                or stock_move.scrapped
+                or (
+                    stock_move.location_dest_id.usage != "customer"
+                    and (
+                        stock_move.location_id.usage != "customer"
+                        or not stock_move.to_refund
                     )
                 )
-                and not mv.scrapped
-                and (
-                    mv.location_dest_id.usage == "customer"
-                    or (mv.location_id.usage == "customer" and mv.to_refund)
-                )
+            ):
+                continue
+            invoice_lines = stock_move.invoice_line_ids.filtered(
+                lambda invl: invl.move_id.state != "cancel"
             )
-        )
+            invoiced_qty = 0
+            for inv_line in invoice_lines:
+                if inv_line.move_id.move_type == "out_refund":
+                    invoiced_qty -= inv_line.quantity
+                else:
+                    invoiced_qty += inv_line.quantity
+            if float_is_zero(
+                invoiced_qty, precision_rounding=self.product_uom.rounding
+            ):
+                moves_linked += stock_move
+        return moves_linked
+        # return self.mapped("move_ids").filtered(
+        #     lambda mv: (
+        #         mv.state == "done"
+        #         and not (
+        #             any(
+        #                 inv.state != "cancel"
+        #                 for inv in mv.invoice_line_ids.mapped("move_id")
+        #             )
+        #         )
+        #         and not mv.scrapped
+        #         and (
+        #             mv.location_dest_id.usage == "customer"
+        #             or (mv.location_id.usage == "customer" and mv.to_refund)
+        #         )
+        #     )
+        # )
+        # <<< QRTL
 
     def _prepare_invoice_line(self, **optional_values):
         vals = super()._prepare_invoice_line(**optional_values)
