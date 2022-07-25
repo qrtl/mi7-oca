1f5c4fdd7a06c276e3119fda75e02dc150a64ee6
Yoshi Tashiro <tashiro@quartile.co>
2022-07-25 08:11:11 +0000

    reflect adhoc 15.0-mig-stock_picking_invoice_link

--- stock_picking_invoice_link/models/stock_move.py
+++ stock_picking_invoice_link/models/stock_move.py
@@ -1,25 +1,37 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2015-2016 AvanzOSC
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2022 Antony Herrera - LooErp
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    # Provide this field for backwards compatibility
     invoice_line_ids = fields.Many2many(
-        comodel_name='account.invoice.line', string='Invoice Lines',
-        compute="_compute_invoice_line_ids")
-    invoice_line_id = fields.Many2one(
-        comodel_name='account.invoice.line', string='Invoice Line',
-        copy=False, readonly=True)
+        comodel_name="account.move.line",
+        relation="stock_move_invoice_line_rel",
+        column1="move_id",
+        column2="invoice_line_id",
+        string="Invoice Line",
+        copy=False,
+        readonly=True,
+    )
 
-    @api.multi
-    @api.depends('invoice_line_id')
-    def _compute_invoice_line_ids(self):
-        for move in self:
-            move.invoice_line_ids = move.invoice_line_id
+    def write(self, vals):
+        """
+        User can update any picking in done state, but if this picking already
+        invoiced the stock move done quantities can be different to invoice
+        line quantities. So to avoid this inconsistency you can not update any
+        stock move line in done state and have invoice lines linked.
+        """
+        if "product_uom_qty" in vals and not self.env.context.get(
+            "bypass_stock_move_update_restriction"
+        ):
+            for move in self:
+                if move.state == "done" and move.invoice_line_ids:
+                    raise UserError(_("You can not modify an invoiced stock move"))
+        return super().write(vals)
