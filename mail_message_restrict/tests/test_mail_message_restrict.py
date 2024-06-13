cc33238b67678a94e91b22ee168132087c8c955a
Aungkokolin1997 <lin@quartile.co>
2024-06-13 10:15:33 +0000

    upd tests

--- mail_message_restrict/tests/test_mail_message_restrict.py
+++ mail_message_restrict/tests/test_mail_message_restrict.py
@@ -6,20 +6,19 @@ from odoo.tests import common
 
 
 class TestMailMessageRestrict(common.TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.MailMessage = self.env["mail.message"]
-        self.MailMessageSubtype = self.env["mail.message.subtype"]
-        self.model_1 = self.env["ir.model"].search([], limit=1)
-        self.model_2 = self.env["ir.model"].search([], offset=1, limit=1)
-
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.message_model = cls.env["mail.message"]
+        cls.model_1 = cls.env["ir.model"].search([], limit=1)
+        cls.model_2 = cls.env["ir.model"].search([], offset=1, limit=1)
         # Setup subtype
-        self.subtype_comment = self.env.ref("mail.mt_comment")
-        self.subtype_comment.allow_send_model_ids = [(6, 0, self.model_1.ids)]
+        cls.subtype_comment = cls.env.ref("mail.mt_comment")
+        cls.subtype_comment.allow_send_model_ids = [(6, 0, cls.model_1.ids)]
 
     def test_create_message_with_comment_type_allowed_model(self):
         # Creating a message with comment type and allowed model
-        self.MailMessage.with_context(test_mail_message_restrict=True).create(
+        self.message_model.with_context(test_mail_message_restrict=True).create(
             {
                 "message_type": "comment",
                 "model": self.model_1.model,
@@ -30,7 +29,7 @@ class TestMailMessageRestrict(common.TransactionCase):
     def test_create_message_with_comment_type_not_allowed_model(self):
         # Creating a message with comment type and not allowed model
         with self.assertRaises(ValidationError):
-            self.MailMessage.with_context(test_mail_message_restrict=True).create(
+            self.message_model.with_context(test_mail_message_restrict=True).create(
                 {
                     "message_type": "comment",
                     "model": self.model_2.model,
