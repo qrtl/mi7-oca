1cfe580736031ff8f78b9f37c789bfab6ed3ca0d
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2025-12-25 20:25:35 +0700

    [6278][ADD] website_form_partner_specific_user_account (#102)

---
+++ website_form_partner_specific_user_account/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Form Partner Specific User Account",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "license": "AGPL-3",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/website",
+    "depends": ["website"],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
+    "installable": True,
+}
