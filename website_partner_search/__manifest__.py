43389c9a3d2ec17dc2b689bba5214e51b4566907
Aungkokolin1997 <lin@quartile.co>
2025-12-25 08:28:07 +0000

    [ADD] website_partner_search

---
+++ website_partner_search/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Website Partner Search",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "license": "LGPL-3",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/website",
+    "depends": ["website"],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
+    "installable": True,
+}
