460e74a6b858cd905c70914e4cd8693a6f16545c
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2026-02-04 16:03:16 +0700

    [6278][IMP] website_form_partner_specific_user_account: reflect oca changes (#104)
    [6278][IMP] website_form_partner_specific_user_account: reflect oca changes (#104)

--- website_form_partner_specific_user_account/controllers/main.py
+++ website_form_partner_specific_user_account/controllers/main.py
@@ -14,32 +14,48 @@ class WebsiteForm(WebsiteForm):
             return super().insert_record(request, model, values, custom, meta)
         Partner = request.env["res.partner"].sudo()
         partner = Partner.browse(partner_id)
-        if not partner.exists():
+        if not partner:
             return super().insert_record(request, model, values, custom, meta)
         email = (
             values.get("email_from")
             or values.get("partner_email")
             or values.get("email")
         )
+        restrict = website.restrict_partner_to_company
+        website_company = website.company_id
+        # Conflicts based on what the partner already has
+        company_conflict = (
+            restrict
+            and bool(partner.company_id)
+            and partner.company_id != website_company
+        )
+        website_conflict = bool(partner.website_id) and partner.website_id != website
+        # If there is any conflict, do not assign missing fields to this partner.
+        # Instead, look for/create a partner that matches website (+company if restricted).
+        if email and (website_conflict or company_conflict):
+            domain = [
+                ("email", "=", email),
+                ("website_id", "=", website.id),
+            ]
+            if website.restrict_partner_to_company:
+                domain.append(("company_id", "=", website.company_id.id))
+            website_partner = Partner.search(domain, limit=1)
+            if not website_partner:
+                vals = {
+                    "email": email,
+                    "name": values.get("partner_name", False),
+                    "website_id": website.id,
+                }
+                if website.restrict_partner_to_company:
+                    vals["company_id"] = website.company_id.id
+                website_partner = Partner.create(vals)
+            if website_partner:
+                values["partner_id"] = website_partner.id
+                partner = website_partner
         # Intended for newly created partners, but applies to any partner without website_id
         if not partner.website_id:
             partner.website_id = website.id
-        if email and partner.website_id != website:
-            website_partner = Partner.search(
-                [
-                    ("email", "=", email),
-                    ("website_id", "=", website.id),
-                ],
-                limit=1,
-            )
-            if not website_partner and values.get("partner_name"):
-                website_partner = Partner.create(
-                    {
-                        "email": email,
-                        "name": values.get("partner_name"),
-                        "website_id": website.id,
-                    }
-                )
-            if website_partner:
-                values["partner_id"] = website_partner.id
+        # Intended for newly created partners, but applies to any partner without company_id
+        if restrict and not partner.company_id:
+            partner.company_id = website_company.id
         return super().insert_record(request, model, values, custom, meta)
